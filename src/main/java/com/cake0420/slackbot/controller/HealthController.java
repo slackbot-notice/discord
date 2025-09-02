dcb1857cd84ceab02d9fa3d4f09d93a2355740f4
soowan <nick6767676767@gmail.com>
2025-09-02 12:59:01 +0900

    feat: 헬스체크 추가

--- src/main/java/com/cake0420/slackbot/controller/HealthController.java
+++ src/main/java/com/cake0420/slackbot/controller/HealthController.java
@@ -1,12 +1,17 @@
 package com.cake0420.slackbot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
 public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<?> health() {
+        log.info("health");
         return ResponseEntity.ok().build();
     }
 }
